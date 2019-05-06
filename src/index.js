1b96f01754f5caac4e5cd07b1bebe5b696d0464a
Andrew Josey <JoganLosey@gmail.com>
2019-05-06 15:52:29 -0400

    added increment and decrement in index.js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,67 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+// State
+// - is always an object (key-value pairs)
+// - is never mutated directly 
+
+const initialState = {
+    value: 1
+};
+
+
+const ACTION_INCREMENT = 'increment';
+const ACTION_DECREMENT = 'decrement';
+
+// Actions
+// 
+function increment() {
+    return {
+        type: ACTION_INCREMENT
+    };
+}
+
+function decrement(){
+    return {
+        type: ACTION_DECREMENT
+    };
+}
+
+window.increment = increment;
+window.decrement = decrement;
+
+// 'increment' or 'decrement'
+
+
+// A reducer
+
+function counter(state=initialState,action) {
+    // inspect the action
+    switch (action.type){
+        case ACTION_INCREMENT:
+        // do some stuff
+        return {
+            value: state.value + 1
+        }
+        break;
+        case ACTION_DECREMENT:
+            return {
+                value: state.value - 1
+            };
+        break;
+
+        default:
+            return state;
+        break; 
+    } 
+
+}
+
+
+window.counter = counter ;
+
+
+// The Store
